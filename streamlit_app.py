b0f0feac78c48f8eb8043b54a4e8b8903d897cb9
Angelquidiante <102175464+Angelquidiante@users.noreply.github.com>
2024-07-15 01:46:34 -0400

    Update streamlit_app.py
    NI IDEA

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Título de la aplicación
+st.title('Visualización de Datos CSV con Streamlit')
+
+# Cargar el archivo CSV
+archivo_csv = st.file_uploader("Cargar archivo CSV", type=['csv'])
+
+if archivo_csv is not None:
+    # Leer el archivo CSV
+    df = pd.read_csv(archivo_csv)
+
+    # Mostrar el DataFrame en Streamlit
+    st.write("Datos del CSV:")
+    st.write(df)
